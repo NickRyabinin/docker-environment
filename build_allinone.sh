b12ec2bbfe441eed3d7a3a3cd11c22f57f3f14f1
Николай <rnn2002@mail.ru>
2024-08-23 19:07:55 +0300

    Remove garbage

--- build_allinone.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./app/src/
-mkdir ./db/
-
-docker build -t allinone -f Dockerfile.allinone .
--- run_allinone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -it \
-  -v $(pwd)/app:/app \
-  -v $(pwd)/db:/var/lib/postgresql/data \
-  -p 80:80 \
-  --name allinone \
-  allinone
