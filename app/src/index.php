3d6fdd21e4e526a414445607e27f68144b672c54
Николай <rnn2002@mail.ru>
2024-08-25 19:30:11 +0300

    Add one more useful info function

--- app/src/index.php
+++ app/src/index.php
@@ -1,3 +1,11 @@
 <?php
 
 phpinfo();
+
+if (function_exists('xdebug_info')) {
+    echo "Xdebug установлен и работает!<br>";
+    echo "Информация о Xdebug:<br>";
+    xdebug_info();
+} else {
+    echo "Xdebug не установлен.<br>";
+}
